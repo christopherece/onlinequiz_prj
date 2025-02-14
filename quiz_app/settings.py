3902c2346f3bf914941ed4fbe2e2626b57f0c6a1
unknown <Administrator@BALAYDALAKAY.LOCAL>
2025-02-15 00:46:42 +1300

    Added webconfig

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rl@l%enghs792d8j!@^w33^!jhkq9&2y@2806oa4xt_)vtd7cm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.11.4','localhost','127.0.0.1','10.210.89.251']
+ALLOWED_HOSTS = ['192.168.11.4','localhost','127.0.0.1','10.210.89.251','192.168.10.250','solvedpad.topitsolutions.co.nz']
 
 
 # Application definition
